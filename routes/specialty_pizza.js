17923e184ebb1782e02a2008efc73e61f359664e
ideans <ideans715@gmail.com>
2016-10-20 10:40:41 -0700

    Edited app.js for our new routes

--- routes/specialty_pizza.js
+++ routes/specialty_pizza.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('route for specialty_pizza');
+router.get( '/', ( request, response ) => {
+  response.send( 'route for specialty_pizza' );
 });
 
 module.exports = router;
