67b989e056365436d86041710eb1e19c99fd41d4
thamaranth <thamaranth@gmail.com>
2016-10-21 12:51:38 -0700

    added customer CRUD

--- database/customersDB.js
+++ database/customersDB.js
@@ -1,1 +1,23 @@
-const db = require('mainDB')
+const db = require( './mainDB' )
+
+
+const customer = {
+
+  add: ( name, address, phone_number ) => db.none( `INSERT INTO customer ( name, address, phone_number ) VALUES ( '${name}', '${address}', '${phone_number}' )` ),
+  getAll: () => db.any( `SELECT * FROM customer` ),
+  getById: id => db.one( `SELECT * FROM customer WHERE id=${id}` ),
+  getNames: () => db.any( `SELECT name FROM customer` ),
+  update: ( id, name = '', address = '', phone_number = '' ) => {
+    let sql = `UPDATE customer SET `
+    if (name != '') sql += `name='${name}'`
+    if (address != '') sql += `, address='${address}'`
+    if (phone_number != '') sql += `, phone_number='${phone_number}'`
+    sql += ` WHERE id = ${id}`
+    db.none( sql ) },
+  delete: id => db.none( `DELETE FROM customer WHERE id=${id}` )
+  
+
+
+}
+
+module.exports = { customer }
---
+++ routes/customer.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../database/mainDB')
+const { customer } = require( '../database/customerDB' )
+
+
+router.get( '/add', ( request, response ) => {
+    response.render('customer/add')
+})
+
+router.post( '/add', ( request, response ) => {
+  const { name, address, phone_number } = request.body
+
+  Promise.all([ customer.add( name, address, phone_number ) ])
+  .then( response.redirect('/customer/') )
+})
+
+router.get( '/', ( request, response ) => {
+  Promise.all([ customer.getAll() ]).then( customer => {
+    response.render('customer/index.pug', { customer: customer[0] })
+  })
+})
+
+router.get( '/edit/:customer_id', ( request, response ) => {
+  const { customer_id } = request.params
+
+  Promise.all([ customer.getById( customer_id ) ]).then( data => {
+    response.render( 'toppings/edit-topping', { customer: data[0]} )
+  })
+})
+
+router.post( '/edit/:customer_id', ( request, response ) => {
+  const { customer_id } = request.params
+  const { name, address, phone_number} = request.body
+
+  Promise.all([ customer.update( customer_id, name, address, phone_number) ])
+  .then( response.redirect( '/customer/' ) )
+
+})
+
+router.get( '/delete/:customer_id', ( request, response ) => {
+  const { customer_id } = request.params
+
+  Promise.all([ customer.getById( customer_id ) ]).then( data => {
+      response.render( 'customer/delete', { customer: data[0] } )
+  })
+})
+
+router.post( '/delete/:customer_id', ( request, response ) => {
+  const { customer_id } = request.params
+
+  Promise.all([ customer.delete( customer_id ) ]).then( data => {
+      response.redirect( '/customer/' )
+  })
+})
+
+module.exports = router;
