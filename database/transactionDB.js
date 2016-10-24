b8f90abf781920287be6a76b433ba4ca4b058b0f
ideans <ideans715@gmail.com>
2016-10-24 12:38:41 -0700

    Full ordering cycle maintains order_id and customer_id and makes all appropriate INSERTS into respective tables.

--- database/transactionDB.js
+++ database/transactionDB.js
@@ -5,7 +5,8 @@ const Transaction = {
   getAll: () => db.any( `SELECT * FROM transaction` ),
   getById: transaction_id => db.one( `SELECT * FROM transaction WHERE id = ${transaction_id}` ),
 
-  new: order_id => db.one( `INSERT INTO transaction ( order_id ) VALUES ( ${order_id} ) RETURNING id` ),
+  new: ( order_id ) => db.one( `INSERT INTO transaction ( order_id ) VALUES ( ${order_id} ) RETURNING id` ),
+  bindCustomer: ( customer_id, transaction_id ) => db.none( `INSERT INTO customer_transactions ( customer_id, transaction_id ) VALUES ( ${customer_id}, ${transaction_id} )` ),
   pay: ( transaction_id, payment_id ) => {
       const date = new Date()
       db.none( `BEGIN TRANSACTION;
--- routes/specialty_pizza.js
+++ routes/specialty_pizza.js
@@ -5,19 +5,21 @@ const { Specialty_pizza } = require('../database/specialty_pizzaDB')
 
 
 router.post( '/details', ( request, response) => {
-  const { order_id, pizza_id } = request.body
+  const { customer_id, order_id, pizza_id } = request.body
   Promise.all([ Specialty_pizza.getById( pizza_id ) ])
   .then( data => {
     response.render( 'specialty_pizza/details', { specialty_pizza: data[0],
+                                                  customer_id: customer_id,
                                                   order_id: order_id } )
   })
 })
 
-router.get ('/add/:order_id' , (request, response) => {
-  const { order_id } = request.params
+router.get ('/add/:customer_id/:order_id' , (request, response) => {
+  const { customer_id, order_id } = request.params
   Promise.all ([ Specialty_pizza.getAll() ] )
   .then( data => {
     response.render ( 'specialty_pizza/add', {specialty_pizza: data[0],
+                                              customer_id: customer_id,
                                               order_id: order_id } )
   })
 })
